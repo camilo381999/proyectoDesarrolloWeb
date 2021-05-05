3930d629e059b2d738dc49ccc3bdf00560b01456
camilo381999 <camilo-leguizamon1@upc.edu.co>
2021-05-05 16:50:46 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include_once('templates/iniciar-html.php');
+?>
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-6 col-sm-12 col-xs-12">
+        </div>
+
+        <div class="col-md-6 col-sm-12 col-xs-12">
+        </div>
+    </div>
+
+</div>
+
+<?php
+include_once('templates/terminar-html.php');
+?>
