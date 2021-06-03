de44790edb104889d3cafa1340eac5894cb503cb
manuel927 <manuel.smo24@gmail.com>
2021-06-03 17:25:11 -0500

    arreglo grafica

--- index.php
+++ index.php
@@ -78,114 +78,112 @@ if (count($porciones) > 1) {
   $hurtos = $datos->selectByLocalidad($localidadDB);
   $modalidad = $datos->selectModalidadByLocalidad($localidadDB);
   $momento = $datos->selectMomentoByLocalidad($localidadDB);
-  $genero=$datos->selectGeneroByLocalidad($localidadDB);
+  $genero = $datos->selectGeneroByLocalidad($localidadDB);
 } else {
   $localidad = "Bogotá";
   $hurtos = $datos->selectAll();
   $modalidad = $datos->selectModalidad();
   $momento = $datos->selectMomento();
-  $genero=$datos->selectGenero();
+  $genero = $datos->selectGenero();
 }
 ?>
 
-<div class="container">
 
-  <div class="bg-color">
-    <div class="part" data-background="#118ab2">
-      <div class="content">
+
+<div class="bg-color">
+  <div class="part" data-background="#118ab2" style="height: 40%;">
+    <div class="container">
+      <!-- <div class="content"> -->
         <h1>Estadísticas de hurto a personas en Bogotá 2021</h1><br>
-        <div class="row">
-          <!-- mapa -->
-          <div class="col-md-6 col-sm-12 col-xs-12">
-            <div id="municipiotxt">Selecciona una localidad</div>
-            <div class="center-block">
-              <div id="map">
-                <script src="js/main.js"></script>
-              </div>
+      <!-- </div> -->
+      
+      <div class="row">
+        <!-- mapa -->
+        <div class="col-md-6 col-sm-12 col-xs-12"><br>
+          <div id="municipiotxt">Selecciona una localidad</div>
+          <div class="center-block">
+            <div id="map">
+              <script src="js/main.js"></script>
+            </div>
+          </div>
+        </div>
+
+        <!-- Otros datos de analisis -->
+        <div class="col-md-6 col-sm-12 col-xs-12">
+          <?php echo '<h3>' . $localidad . '</h3>'; ?><br>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Modalidad de hurto</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Las 3 modalidades más comunes de hurto en <?php echo $localidad; ?> son: </h6>
+              <?php
+              $sum = 0;
+              foreach ($modalidad as $unidad) {
+                $sum = $sum + $unidad[1];
+              }
+              $dato = 100 / $sum;
+              ?>
+              <p class="card-text">1. <?php echo $modalidad[0][0] . " con una frecuencia del " . number_format($dato * $modalidad[0][1], 2) . "%"; ?></p>
+              <p class="card-text">2. <?php echo $modalidad[1][0] . " con una frecuencia del " . number_format($dato * $modalidad[1][1], 2) . "%"; ?></p>
+              <p class="card-text">3. <?php echo $modalidad[2][0] . " con una frecuencia del " . number_format($dato * $modalidad[2][1], 2) . "%"; ?></p>
             </div>
           </div>
 
-          <!-- Otros datos de analisis -->
-          <div class="col-md-6 col-sm-12 col-xs-12">
-            <?php echo '<h3>' . $localidad . '</h3>'; ?><br>
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Modalidad de hurto</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Las 3 modalidades más comunes de hurto en <?php echo $localidad; ?> son: </h6>
-                <?php
-                $sum=0;
-                foreach($modalidad as $unidad){
-                  $sum=$sum+$unidad[1];
-                }
-                $dato = 100/$sum;
-                ?>
-                <p class="card-text">1. <?php echo $modalidad[0][0]." con una frecuencia del ".number_format($dato*$modalidad[0][1], 2)."%"; ?></p>
-                <p class="card-text">2. <?php echo $modalidad[1][0]." con una frecuencia del ".number_format($dato*$modalidad[1][1], 2)."%"; ?></p>
-                <p class="card-text">3. <?php echo $modalidad[2][0]." con una frecuencia del ".number_format($dato*$modalidad[2][1], 2)."%"; ?></p>
-              </div>
+          <br>
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Momento del día</h5>
+              <?php
+              $sum = 0;
+              foreach ($momento as $unidad) {
+                $sum = $sum + $unidad[1];
+              }
+              $dato = 100 / $sum;
+              ?>
+              <h6 class="card-subtitle mb-2 text-muted">El <?php echo number_format($dato * $momento[0][1], 2) . "%"; ?> de los hurtos que se presentan en <?php echo $localidad; ?> ocurren en la: </h6>
+              <p class="card-text"><?php echo $momento[0][0]; ?></p>
             </div>
+          </div>
 
-            <br>
+          <br>
 
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Momento del día</h5>
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Género mas afectado</h5>
+              <h6 class="card-subtitle mb-2 text-muted">El
                 <?php
-                $sum=0;
-                foreach($momento as $unidad){
-                  $sum=$sum+$unidad[1];
-                }
-                $dato = 100/$sum;
-                ?>
-                <h6 class="card-subtitle mb-2 text-muted">El <?php echo number_format($dato*$momento[0][1], 2)."%"; ?> de los hurtos que se presentan en <?php echo $localidad; ?> ocurren en la: </h6>
-                <p class="card-text"><?php echo $momento[0][0]; ?></p>
-              </div>
-            </div>
-
-            
-            <br>
-
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Género mas afectado</h5>
-                <h6 class="card-subtitle mb-2 text-muted">El 
-                <?php 
-                $a=$genero[0][1];
-                $b=$genero[1][1];
-                $sum=$a+$b;
-                $dato = 100/$sum;
-                echo number_format($dato*$a, 2);
+                $a = $genero[0][1];
+                $b = $genero[1][1];
+                $sum = $a + $b;
+                $dato = 100 / $sum;
+                echo number_format($dato * $a, 2);
                 ?>% de los hurtos en <?php echo $localidad; ?> fueron a personas del genero: </h6>
-                <p class="card-text"><?php echo $genero[0][0]; ?></p>
-              </div>
+              <p class="card-text"><?php echo $genero[0][0]; ?></p>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="part" data-background="#fff">
-      <div class="content">
-        <div class="row">
-          <!-- Grafica de barras -->
-          <div class="col-md-12 col-sm-12 col-xs-12"><br><br>
-            <div class="grafica">
-              <div class="chart-container" style="position: relative; height:600px; width:800px">
-                <canvas id="myChart"></canvas>
-              </div>
+      <div class="row">
+        <!-- Grafica de barras -->
+        <div class="col-md-12 col-sm-12 col-xs-12"><br><br>
+          <div class="grafica">
+            <div class="chart-container" style="position: relative; height:600px; width:800px">
+              <canvas id="myChart"></canvas>
             </div>
           </div>
         </div>
       </div>
+
     </div>
+  </div>
+
+  <div class="part" data-background="#fff" style="height: 60%;">
 
   </div>
 
-
-
-
-
 </div>
+
 
 <script>
   const ylabels = [];
@@ -232,6 +230,6 @@ if (count($porciones) > 1) {
 
 
 <?php
-echo $localidad;
+//echo $localidad;
 include_once('templates/terminar-html.php');
 ?>
